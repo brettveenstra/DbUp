0218aeaf9151f6e213799b31412e15cdddd5796d
Brett Veenstra <brettveenstra@gmail.com>
2019-05-19 08:08:34 -0400

    add Tests describing expected behavior

--- src/dbup-tests/Support/Oracle/OracleConnectionManagerTests.cs
+++ src/dbup-tests/Support/Oracle/OracleConnectionManagerTests.cs
@@ -34,5 +34,59 @@ namespace DbUp.Tests.Support.Oracle
 
             result.Count().ShouldBe(2);
         }
+
+        [Fact]
+        public void CanParseBlockTerminatorSingleStatement()
+        {
+            var singleCommand = "create table FOO (myid INT NOT NULL);";
+            singleCommand += Environment.NewLine;
+            singleCommand += "/";
+            
+            var connectionManager = new OracleConnectionManager("connectionstring");
+            var result = connectionManager.SplitScriptIntoCommands(singleCommand);
+
+            result.Count().ShouldBe(1);
+
+        }
+        
+        [Fact]
+        public void CanParseBlockTerminatorOnlyForBlankLine()
+        {
+            var singleCommand = "create table FOO (myid INT NOT NULL);/";
+            singleCommand += Environment.NewLine;
+            singleCommand += "/";
+            
+            var connectionManager = new OracleConnectionManager("connectionstring");
+            var result = connectionManager.SplitScriptIntoCommands(singleCommand);
+
+            result.Count().ShouldBe(1);
+        }      
+        
+        [Fact]
+        public void CanParseBlockTerminatorMultipleStatements()
+        {
+            var multiCommand = "create table FOO (myid INT NOT NULL);/";
+            multiCommand += Environment.NewLine;
+            multiCommand += "/";
+            multiCommand += Environment.NewLine;
+            multiCommand += @"SELECT *
+FROM user_tables
+WHERE table_name ='FOO'";        // NOTE: leaving off ';'
+            multiCommand += Environment.NewLine;
+            multiCommand += "/";
+            multiCommand += Environment.NewLine;
+            multiCommand += "create table BAR (myid INT NOT NULL);";
+            multiCommand += Environment.NewLine;
+            multiCommand += "/";
+            multiCommand += Environment.NewLine;
+            multiCommand += @"SELECT *
+FROM user_tables
+WHERE table_name ='BAR';";        // NOTE: including ';'
+
+            var connectionManager = new OracleConnectionManager("connectionstring");
+            var result = connectionManager.SplitScriptIntoCommands(multiCommand);
+
+            result.Count().ShouldBe(4);
+        }  
     }
 }
